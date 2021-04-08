881c28c67aafc3711dd7512d85f3511649fc6227
anupam-code <58290111+anupam-code@users.noreply.github.com>
2021-04-08 16:03:20 +0530

    Update demo.c

--- demo.c
+++ demo.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 int fun(int a)
-{  if (a == 10)
+{ /* if (a == 10)
 	return 10;
 	else if(a ==20)
       	return 20;
-   // return 0;
+  */
+	// return 0;
 }
 int main()
 {   int a = fun(30);
